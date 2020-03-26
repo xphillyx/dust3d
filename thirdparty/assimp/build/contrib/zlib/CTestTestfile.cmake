c53b24db5107701c7ab7ab450ce85a21db928422
huxingyi <huxingyi@msn.com>
2020-03-26 22:58:55 +0930

    Remove build folder [skip ci]

--- thirdparty/assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jhu/sideprojects/dust3d/thirdparty/assimp/assimp-5.0.1/contrib/zlib
-# Build directory: C:/Users/jhu/sideprojects/dust3d/thirdparty/assimp/assimp-5.0.1/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
