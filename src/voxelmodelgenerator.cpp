93b0aee49658042a480ced7489ca146e33fa435b
huxingyi <huxingyi@msn.com>
2020-06-08 11:29:25 +0930

    Optimize model generation [skip ci]

--- src/voxelmodelgenerator.cpp
+++ src/voxelmodelgenerator.cpp
@@ -34,9 +34,9 @@ Model *VoxelModelGenerator::takeModel()
 
 struct MeshToModel
 {
-    MeshToModel(std::vector<QVector3D> *vertices, std::vector<std::vector<size_t>> *triangles, ShaderVertex *target):
+    MeshToModel(std::vector<QVector3D> *vertices, std::vector<std::vector<size_t>> *quads, ShaderVertex *target):
         voxelVertices(vertices),
-        voxelTriangles(triangles),
+        voxelQuads(quads),
         triangleVertices(target)
     {
     }
@@ -45,40 +45,54 @@ struct MeshToModel
     float clayG = Theme::clayColor.greenF();
     float clayB = Theme::clayColor.blueF();
     
+    int facePointIndices[2][3] = {
+        {0, 1, 2},
+        {2, 3, 0}
+    };
+    
     std::vector<QVector3D> *voxelVertices = nullptr;
-    std::vector<std::vector<size_t>> *voxelTriangles = nullptr;
+    std::vector<std::vector<size_t>> *voxelQuads = nullptr;
     ShaderVertex *triangleVertices = nullptr;
     
-    void operator()(const tbb::blocked_range<size_t> &range) const {
+    inline void addTriangleVertex(int vertexIndex, const QVector3D &srcNormal, int destTriangleIndex) const
+    {
+        const QVector3D *srcVert = &(*voxelVertices)[vertexIndex];
+        ShaderVertex *dest = &triangleVertices[destTriangleIndex];
+        dest->colorR = clayR;
+        dest->colorG = clayG;
+        dest->colorB = clayB;
+        dest->alpha = 1.0;
+        dest->posX = srcVert->x();
+        dest->posY = srcVert->y();
+        dest->posZ = srcVert->z();
+        dest->texU = 0;
+        dest->texV = 0;
+        dest->normX = srcNormal.x();
+        dest->normY = srcNormal.y();
+        dest->normZ = srcNormal.z();
+        dest->metalness = Model::m_defaultMetalness;
+        dest->roughness = Theme::clayRoughness;
+        dest->tangentX = 0;
+        dest->tangentY = 0;
+        dest->tangentZ = 0;
+    }
+    
+    void operator()(const tbb::blocked_range<size_t> &range) const 
+    {
         for (size_t i = range.begin(); i != range.end(); ++i) {
-            const auto &facePoints = (*voxelTriangles)[i];
+            const auto &facePoints = (*voxelQuads)[i];
             const auto srcNormal = normalOfThreePointsHighPrecision((*voxelVertices)[facePoints[0]],
                 (*voxelVertices)[facePoints[1]],
                 (*voxelVertices)[facePoints[2]]);
-            int destIndex = i * 3;
-            for (auto j = 0; j < 3; j++) {
-                int vertexIndex = (*voxelTriangles)[i][j];
-                const QVector3D *srcVert = &(*voxelVertices)[vertexIndex];
-                ShaderVertex *dest = &triangleVertices[destIndex];
-                dest->colorR = clayR;
-                dest->colorG = clayG;
-                dest->colorB = clayB;
-                dest->alpha = 1.0;
-                dest->posX = srcVert->x();
-                dest->posY = srcVert->y();
-                dest->posZ = srcVert->z();
-                dest->texU = 0;
-                dest->texV = 0;
-                dest->normX = srcNormal.x();
-                dest->normY = srcNormal.y();
-                dest->normZ = srcNormal.z();
-                dest->metalness = Model::m_defaultMetalness;
-                dest->roughness = Theme::clayRoughness;
-                dest->tangentX = 0;
-                dest->tangentY = 0;
-                dest->tangentZ = 0;
-                destIndex++;
-            }
+            int destTriangleIndex = i * 6;
+            
+            addTriangleVertex(facePoints[0], srcNormal, destTriangleIndex++);
+            addTriangleVertex(facePoints[1], srcNormal, destTriangleIndex++);
+            addTriangleVertex(facePoints[2], srcNormal, destTriangleIndex++);
+            
+            addTriangleVertex(facePoints[2], srcNormal, destTriangleIndex++);
+            addTriangleVertex(facePoints[3], srcNormal, destTriangleIndex++);
+            addTriangleVertex(facePoints[0], srcNormal, destTriangleIndex++);
         }
     }
 };
@@ -92,18 +106,39 @@ void VoxelModelGenerator::generate()
 	timer.start();
 
 	std::vector<QVector3D> voxelVertices;
-	std::vector<std::vector<size_t>> voxelTriangles;
-	m_voxelGrid->toMesh(&voxelVertices, &voxelTriangles);
+	std::vector<std::vector<size_t>> voxelQuads;
+    
+    std::vector<openvdb::Vec3s> points;
+	std::vector<openvdb::Vec3I> triangles;
+	std::vector<openvdb::Vec4I> quads;
+	
+    double isovalue = 0.0;
+	double adaptivity = 0.0;
+	bool relaxDisorientedTriangles = false;
+	openvdb::tools::volumeToMesh<openvdb::FloatGrid>(*m_voxelGrid->m_grid, points, triangles, quads,
+		isovalue, adaptivity, relaxDisorientedTriangles);
+        
+    voxelVertices.resize(points.size());
+	for (size_t i = 0; i < points.size(); ++i) {
+		const auto &src = points[i];
+		voxelVertices[i] = QVector3D(src.x(), src.y(), src.z());
+	}
+    
+    voxelQuads.resize(quads.size());
+    for (size_t i = 0; i < quads.size(); ++i) {
+        const auto &src = quads[i];
+		voxelQuads[i] = std::vector<size_t> {src.x(), src.w(), src.z(), src.y()};
+	}
     
     auto createMeshStartTime = timer.elapsed();
 
-    int triangleVertexCount = voxelTriangles.size() * 3;
+    int triangleVertexCount = voxelQuads.size() * 3 * 2;
     ShaderVertex *triangleVertices = new ShaderVertex[triangleVertexCount];
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, voxelTriangles.size()),
-        MeshToModel(&voxelVertices, &voxelTriangles, triangleVertices));
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, voxelQuads.size()),
+        MeshToModel(&voxelVertices, &voxelQuads, triangleVertices));
     
     m_model = new Model(triangleVertices, triangleVertexCount, 0, 0,
-        &voxelVertices, &voxelTriangles);
+        &voxelVertices, &voxelQuads);
     
     auto createMeshConsumedTime = timer.elapsed() - createMeshStartTime;
 	qDebug() << "VOXEL createMesh took milliseconds:" << createMeshConsumedTime;
